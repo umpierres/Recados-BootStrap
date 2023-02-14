d213a83fe60ba2f517bd81ec08e48f43522943bf
umpierres <alexandraumpierres@outlook.com>
2023-02-14 13:39:04 -0300

    adicionando modal e validação de cadastro

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-const toastDiv = document.getElementById('toast-alert');
-const toast = new bootstrap.Toast(toastDiv);
+
+const usuarioLogado = buscarDadosLocalStorage('usuarioLogado');
+const listaCadastros = buscarDadosLocalStorage('cadastrosUsuarios');
 document.addEventListener('DOMContentLoaded', () => {
     const usuarioLogado = localStorage.getItem('usuarioLogado');
     if (!usuarioLogado) {
         window.location.href = './index.html';
+    }else {
+        mostrarRegistrosHTML();
     }
 }); 
+
+const toastDiv = document.getElementById('toast-alert');
+const toast = new bootstrap.Toast(toastDiv);
+const tbody = document.querySelector('#tabelaBody');
+const index = listaCadastros.findIndex(
+    (usuario) => usuario.id === usuarioLogado.id
+);
+const modalCriar = new bootstrap.Modal('#modal-criar')
+
+const formCriar = document.getElementById('form-criar')
+
+formCriar.addEventListener('submit', (event => {
+    event.preventDefault();
+    if(listaCadastros[index].recados.length > 25){
+        toastShow('danger','Numero de recados excedidos')
+        modalCriar.hide()
+        return
+    }
+    if(!formCriar.checkValidity()) {
+        formCriar.classList.add('was-validated')
+        return
+    }
+
+    const tituloRecado = document.querySelector('#tituloRecado').value;
+    const mensagemRecado = document.querySelector('#mensagemRecado').value;
+    listaCadastros[index].recados.push({
+        titulo: tituloRecado,
+        mensagem: mensagemRecado,
+        id: gerarId()
+    });
+
+    guardarDadosLocalStorage('cadastrosUsuarios', listaCadastros);
+    modalCriar.hide()
+
+    formCriar.classList.remove('was-validated')
+    formCriar.reset()
+    mostrarRegistrosHTML();
+    toastShow('success', 'Recado criado com sucesso!')
+}))
+
+
+
+
+
+
+
+
+
 
 function gerarId() {
     return new Date().getTime();
@@ -42,4 +93,42 @@ function buscarDadosLocalStorage(chave) {
     } else {
         return [];
     }
+}
+
+function mostrarRegistrosHTML() {
+    tbody.innerHTML = ''
+    listaCadastros[index].recados.forEach((valor) => {
+        tbody.innerHTML += `
+        <tr id="${valor.id}">
+        
+            
+            
+            <td>${valor.titulo}</td>
+            <td>${valor.mensagem}</td>
+
+            <td>
+                <button
+                    class='border-0 bg-body'
+                    data-id="${valor.id}"
+                     onclick="editarRecado(this)" 
+                >
+                    <i
+                        class="bi bi-pencil-square text-success"
+                    ></i>
+                </button>
+                <button
+                class='border-0 bg-body'
+                    data-id="${valor.id}"
+                    
+                    onclick="apagarRecado(this)" 
+                >
+                    <i
+                        class="bi bi-trash-fill text-danger"
+                    ></i>
+                </button>
+            </td>
+    
+        </tr>
+        `;
+    });
 }
